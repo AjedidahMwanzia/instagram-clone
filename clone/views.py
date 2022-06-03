231a2310ad08423a63baecacdaf0194aea79843d
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-04 01:47:40 +0300

    create class views inviews.py

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import (
+    ListView
+)
 # Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = "posts"
+    template_name='clone/post_list'
