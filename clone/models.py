d3feda2eaa7f9930b88d53a40c9976c51b75f90c
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-05 22:56:25 +0300

    add media files and update models

--- clone/models.py
+++ clone/models.py
@@ -4,9 +4,12 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author=models.ForeignKey('auth.User',on_delete = models.CASCADE)
-    image=models.ImageField(blank=True,null=True)
+    image=models.ImageField(default='DEFAULT VALUE',blank=True,null=True)
     caption = models.TextField()
     created_on=models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.caption
+
+    def save_image(self):
+        self.save()
--- instagram/settings.py
+++ instagram/settings.py
@@ -117,9 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+MEDIA_URL='/image/'
+
+STATICFILES_DIRS=[
+    BASE_DIR /'static'
 ]
+
+MEDIA_ROOT= BASE_DIR/'static/image'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
