86b9e6ff90a9e8a64ea80e4fdb26c1aae5db8ce7
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-06 22:36:38 +0300

    add the profile and authentication pages

--- clone/urls.py
+++ clone/urls.py
@@ -8,9 +8,12 @@ urlpatterns=[
   path('search/', views.search_results, name='search_results'),
   path('', views.home, name='home'),
   path('user/<user_id>', views.profile, name='profile'),
-   path('new/', views.post_create,name="post_create"),
+  path('new/', views.post_create,name="post_create"),
   path('user/update/profile', views.update_profile, name='updateprofile'),
   path('post/<image_id>',views.post_detail,name='post_detail'),
   path('post/<image_id>/like',views.like_image,name='likeimage')
+  path('profile/<username>/', views.profile, name='user_profile'),
+  path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+  path('follow/<to_follow>', views.follow, name='follow')
   
 ]
